d4d45156b692d35e1d383445dc13a3dbeb377940
khe4oyan <khe4oyan@gmail.com>
2023-10-29 15:12:27 +0400

    fix

--- client/js/components/postsTypes/Paragraph.js
+++ client/js/components/postsTypes/Paragraph.js
@@ -2,6 +2,6 @@ import { DOM } from "../../tools/dom.js";
 
 export default function Paragraph(data) {
 	const p = DOM.CE('p', 'post__paragraph');
-	p.innerText = data;
+	p.innerHTML = data;
 	return p;
 }
