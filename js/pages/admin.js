f1359a8232ba585ae97771656df55795898ddcc7
khe4oyan <khe4oyan@gmail.com>
2023-11-01 16:50:43 +0400

    admin.js

--- js/pages/admin.js
+++ js/pages/admin.js
@@ -1,5 +1,32 @@
-import {DOM } from '../tools/dom.js';
+import { DOM } from '../tools/dom.js';
+import {DB, postTypes} from '../data/DB.js'
 
 {
-    
+    const createPreview = (previewData) => {
+        // switch case previewData.type, create element and return it
+        const elem = DOM.CE('div');
+        elem.innerText = 'previewData';
+
+        return elem;
+    }
+
+    const createNewData = () => {
+        const newData = DOM.CE('div', 'newData');
+        const closeButton = DOM.CE('button', 'closeButton', newData);
+        closeButton.innerText = 'x';
+
+        const dataTypesList = DOM.CE('select', 'dataTypes', newData);
+        dataTypesList.name = 'dataTypeList';
+        // add option in to this dataTYpesList
+        
+        const inputData = DOM.CE('input', 'inputData', newData);
+        inputData.placeholder = 'Enter data'
+
+        const addButton = DOM.CE('button', 'addButton', newData);
+        addButton.innerText = 'add';
+
+        return newData;
+    }
+
+
 }
