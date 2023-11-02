da0d801370def6d7adf58d2d53d742bca380140c
khe4oyan <khe4oyan@gmail.com>
2023-11-02 14:36:36 +0400

    add compiler

--- js/pages/admin/admin.js
+++ js/pages/admin/admin.js
@@ -1,70 +1,13 @@
 import { DOM } from '../../tools/dom.js';
-import {DB, postTypes} from '../../data/DB.js'
-import Tool from './tool.js'
+import Compiler from './tool.js'
 
 {
-    const previewDataArray = [];
+  const textArea = DOM.GE('.admin .text');
+  // textArea.addEventListener('input', () => {
+  //   new Compiler(textArea.value.split('\n'));
+  // });
 
-    const previewPush = (data) => {
-        preview.push(data);
-
-        const previewDOM = DOM.GE('.preview');
-        previewDOM.innerHTML = '';
-        for (let i = 0; i < preview.length; ++i) {
-            previewDOM.appendChild(Tool.createPreview(preview[i], postTypes));
-        }
-    }
-
-    const createNewData = () => {
-        const newData = DOM.CE('div', 'newData');
-        const closeButton = DOM.CE('button', 'closeButton', newData);
-        closeButton.innerText = 'x';
-        closeButton.addEventListener('click', () => {
-            newData.remove();
-        });
-
-        const dataTypesList = DOM.CE('select', 'dataTypes', newData);
-        dataTypesList.name = 'dataTypeList';
-
-        {
-            const option = DOM.CE('option', null, dataTypesList);
-            option.hidden=true;
-            option.innerText = "(select data type)";
-            
-            const typeList = Object.keys(postTypes);
-            for (let i = 0; i < typeList.length; ++i) {
-                const option = DOM.CE('option', null, dataTypesList);
-                option.value = postTypes[typeList[i]];
-                option.innerText = postTypes[typeList[i]];
-            }
-        }
-        
-        const inputData = DOM.CE('input', 'inputData', newData);
-        inputData.placeholder = 'Enter data'
-
-        const addButton = DOM.CE('button', 'addButton', newData);
-        addButton.innerText = 'add';
-        addButton.addEventListener('click', () => {
-            const post = {
-                id: Math.random() + '/' + new Date().getTime(),
-                content: inputData.value,
-                type: dataTypesList.value,
-            };
-
-            previewPush(post);
-            newData.remove();
-        });
-
-        DOM.GE('.inputs').appendChild(newData);
-    }
-
-    const postTitle = DOM.CE('input', 'postTitle');
-    postTitle.placeholder = 'Post title';
-    DOM.GE('.admin .container').prepend(postTitle);
-
-    const createPostButton = DOM.GE('.createPost');
-    const createDataType = DOM.GE('.createDataType');
-    createDataType.addEventListener('click', () => {
-        createNewData();
-    });
+  const compileButton = DOM.GE('.admin .createPost');
+  compileButton.addEventListener('click', () => new Compiler(textArea.value.split('\n')));
+  compileButton.click();
 }
--- js/pages/admin/tool.js
+++ js/pages/admin/tool.js
@@ -5,28 +5,72 @@ import Paragraph from '../../components/postsTypes/Paragraph.js'
 import PointList from '../../components/postsTypes/PointList.js'
 import Youtube from '../../components/postsTypes/Youtube.js'
 
-class Tool {
-	constructor() { throw new Error('not instance'); }
-	
-	static createPreview(previewData, postTypes) {
-		const elem = DOM.CE('div');
+class Compiler {
+	#intermediateLines = [];
+	#convertedDataType = [];
+	#previewDOM = document.querySelector('.preview');
+	#lineIndex = 0;
 
-		let apenedElem = null;
+	constructor(intermediateLines) {
+		this.#previewDOM.innerHTML = '';
+		this.#whiteSpaceDelete(intermediateLines);
+		this.#checkingEveryLine();
+	}
 
-		console.log(previewData.type);
-		switch (previewData.type) {
-			case postTypes.headerText: { apenedElem = HeaderText(previewData.content); break; }
-			case postTypes.image: { apenedElem = Image(previewData.content); break; }
-			case postTypes.numberList: { apenedElem = NumberList(previewData.content); break; }
-			case postTypes.paragraph: { apenedElem = Paragraph(previewData.content); break; }
-			case postTypes.pointList: { apenedElem = PointList(previewData.content); break; }
-			case postTypes.youtube: { apenedElem = Youtube(previewData.content); break; }
+	#whiteSpaceDelete(intermediateLines) {
+		for (let i = 0; i < intermediateLines.length; ++i) {
+			if (intermediateLines[i] === '') { continue; }
+			this.#intermediateLines.push(intermediateLines[i]);
 		}
+	}
 
-		elem.appendChild(apenedElem);
+	#checkingEveryLine() {
+		for (this.#lineIndex; this.#lineIndex < this.#intermediateLines.length; ++this.#lineIndex) {
+			const i = this.#lineIndex;
+			
+			switch(this.#intermediateLines[i][0]) {
+				case '#': { this.#textHeader(this.#intermediateLines[i].split('# ')[1]); break;}
+				case '!': { this.#imgOrVid(this.#intermediateLines[i]); break;}
+				case '-': { this.#list('-', NumberList); break;}
+				case '*': { this.#list('*', PointList); break;}
+				default: this.#paragraph();
+			}
+		}
+	}
 
-		return elem;
+	#textHeader(text) {
+		this.#append(HeaderText(text), `HT('${text}')`);
+	}
+
+	#imgOrVid(text) {
+		text = text.split('!')[1];
+
+		if (text.includes('.png')) {
+			this.#append(Image(text), `IMG('${text}')`);
+		} else {
+			this.#append(Youtube(text), `YT('${text}')`);
+		}
+	}
+
+	#list(char, component) {
+		const list = [];
+
+		do {
+			list.push(this.#intermediateLines[this.#lineIndex].split(`${char} `)[1]);
+			++this.#lineIndex;
+		}while(this.#lineIndex < this.#intermediateLines.length && this.#intermediateLines[this.#lineIndex][0] === char);
+
+		this.#append(component(list), 'change');
+	}
+
+	#paragraph() {
+		
+	}
+
+	#append(elem, data) {
+		this.#previewDOM.appendChild(elem);
+		this.#convertedDataType.push(data);
 	}
 };
 
-export default Tool;
+export default Compiler;
